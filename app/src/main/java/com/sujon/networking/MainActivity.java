1b3e170b4a48d0a320c75fc26986c080e2eef236
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-07 02:26:01 +0600

    Revert "2_Android Volley - Android Development with Java - Rakibul Islam - Bohubrihi"
    This reverts commit 7153dfa532f149d7202b7ae99dd27654fd672917.

--- app/src/main/java/com/sujon/networking/MainActivity.java
+++ app/src/main/java/com/sujon/networking/MainActivity.java
@@ -1,22 +1,13 @@
 package com.sujon.networking;
 
-import android.widget.Button;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView myTextView;
-    Button sendButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        myTextView = findViewById(R.id.textView);
-        sendButton = findViewById(R.id.button);
-
     }
 }
