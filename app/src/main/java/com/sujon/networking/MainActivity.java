ad7ee80aa71bf1199bec2f8279aadd21a53a4757
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-07 02:50:30 +0600

    volley
    volley

--- app/src/main/java/com/sujon/networking/MainActivity.java
+++ app/src/main/java/com/sujon/networking/MainActivity.java
@@ -1,13 +1,47 @@
 package com.sujon.networking;
 
+import android.app.DownloadManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView myTextView;
+    Button sendButton;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myTextView = findViewById(R.id.textView);
+        sendButton = findViewById(R.id.button);
+        queue = Volley.newRequestQueue(this);
+
+    }
+    public void SendRequest(View view){
+        String url ="https://raykibul.com/api/get.php";
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                myTextView.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                myTextView.setText(error.toString());
+            }
+        });
+        queue.add(request);
     }
 }
